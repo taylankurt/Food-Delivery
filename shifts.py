6c8a2d65d413a7d2c77b75986dcd1ed077ecc438
Taylan Kurt <kurt.taylan@icloud.com>
2022-11-02 21:00:16 +0100

    comments added to shifts.py for better readability, file 'goals and possibilities' created to write down the goals ideas

--- shifts.py
+++ shifts.py
@@ -1,13 +1,11 @@
-import locale
 from datetime import datetime as dt, date
 import calendar as cal
 
-#`locale.setlocale(locale.LC_ALL, 'de_DE')
 date_today = dt.now()
 current_date = date.today()
 current_month_array = cal.monthcalendar(year = current_date.year, month = current_date.month)
 
-def last_day():
+def last_day(): # to determine the last day of the month
     largest_number = current_month_array[0][0]
     for entry in current_month_array:
         for x in entry:
@@ -15,7 +13,7 @@ def last_day():
                 largest_number = x
     return largest_number
 
-def today_decimal():
+def today_decimal(): # for range 
     return int(date_today.strftime("%d"))
 
 def month_first_day ():
@@ -29,19 +27,19 @@ def month_last_day ():
 def month_day(day):
     return date_today.replace(day = day)
 
-def km_order_data():
-    days = []
-    total_km = 0
-    total_orders = 0
+def current_distance_delivery():
+    shift_days = []
+    total_distance = 0
+    total_deliveries = 0
     for x in range(month_first_day().day, today_decimal() + 1):
         x = month_day(x).strftime("%A %d, %B %Y")
         if "Monday" in x or "Tuesday" in x or "Wednesday" in x or "Friday" in x or "Saturday" in x:
-            days.append(x)
-    for y in days:
-        daily_km = float(input("How much did you ride on {}: ".format(y)))
-        daily_orders = float(input("How many delivers did you had on {}: ".format(y)))
-        total_km = daily_km + total_km
-        total_orders = daily_orders + total_orders
-    return ("You have driven until {} {}km and had delivered {} orders".format(date_today.strftime("%A %d, %B %Y"), (total_km),(total_orders)))
+            shift_days.append(x)
+    for y in shift_days:
+        daily_distance = float(input("How much did you ride on {}: ".format(y)))
+        daily_delivery = float(input("How many delivers did you had on {}: ".format(y)))
+        total_distance = daily_distance + total_distance
+        total_deliveries = daily_delivery + total_deliveries
+    return ("You have driven until {} {}km and had delivered {} orders".format(date_today.strftime("%A %d, %B %Y"), total_distance,total_deliveries))
 
-print(km_order_data())
+print(current_distance_delivery())
