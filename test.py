2ca352879718637d2d62545491787eb5125ca24c
Taylan Kurt <kurt.taylan@icloud.com>
2022-11-18 17:07:40 +0100

    changes in experiment.py are copied to test.py

--- test.py
+++ test.py
@@ -6,29 +6,30 @@ shifts = []
 
 
 class Month:
-    def __init__(self, month_number=1):
-        self.month = month_number
+    def __init__(self):
+        self.month_number = int(input(
+            "Please enter the month in number(1-12): "))
 
     def today_decimal(self):
         date_today = dt.now()
         return int(date_today.strftime("%d"))
 
     def first_day_decimal(self):
-        month = self.month
+        month = self.month_number
         date_today = dt.now()
         date_x = date_today.replace(month=month, day=1)
         return date_x.day
 
     def first_day(self, day):
         self.day = day
-        month = self.month
+        month = self.month_number
         date_today = dt.now()
         date_x = date_today.replace(
             month=month, day=day).strftime("%A %d, %B %Y")
         return date_x
 
     def last_day_decimal(self):  # to determine the last day of the month
-        month = self.month
+        month = self.month_number
         current_date = date.today()
         current_month_array = cal.monthcalendar(
             year=current_date.year, month=month)
@@ -39,51 +40,27 @@ class Month:
                     largest_number = x
         return largest_number
 
-    # def shifts_month(self):
-    #     for x in range(month.first_day_decimal(), month.today_decimal() + 1):
-    #         x = month.first_day(x)
-    #         if "Monday" in x or "Tuesday" in x or "Wednesday" in x or "Friday" in x or "Saturday" in x:
-    #             shifts.append(x)
-    #     return shifts
+    def shifts_month(self):
+        for decimal_day in range(self.first_day_decimal(), self.today_decimal() + 1):
+            fullday = self.first_day(decimal_day)
+            if "Monday" in fullday or "Tuesday" in fullday or "Wednesday" in fullday or "Friday" in fullday or "Saturday" in fullday:
+                shifts.append(fullday)
+        return shifts
 
     def current_distance_delivery(self):
-        month = self.month
         date_today = dt.now()
-        os.system("cls")
+        os.system("clear")
         total_distance = 0
         total_deliveries = 0
-        for x in shifts_month():
+        for x in self.shifts_month():
             daily_distance = float(
                 input("How much did you ride on {}: ".format(x)))
             daily_delivery = float(
                 input("How many delivers did you had on {}: ".format(x)))
             total_distance = daily_distance + total_distance
             total_deliveries = daily_delivery + total_deliveries
-        return ("You have driven until {} {}km and had delivered {} orders".format(date_today.replace(month=month).strftime("%A %d, %B %Y"), total_distance, int(total_deliveries)))
-
-
-def shifts_month():
-    selected_month = int(input("Please enter the month in number(1-12): "))
-    month = Month(selected_month)
-    for x in range(month.first_day_decimal(), month.last_day_decimal() + 1):
-        x = month.first_day(x)
-        if "Monday" in x or "Tuesday" in x or "Wednesday" in x or "Friday" in x or "Saturday" in x:
-            shifts.append(x)
-    return shifts
-
-
-# def current_distance_delivery():
-#     os.system("cls")
-#     total_distance = 0
-#     total_deliveries = 0
-#     for x in shifts_month():
-#         daily_distance = float(
-#             input("How much did you ride on {}: ".format(x)))
-#         daily_delivery = float(
-#             input("How many delivers did you had on {}: ".format(x)))
-#         total_distance = daily_distance + total_distance
-#         total_deliveries = daily_delivery + total_deliveries
-#     return ("You have driven until {} {}km and had delivered {} orders".format(date_today.strftime("%A %d, %B %Y"), total_distance, int(total_deliveries)))
+        return ("You have driven until {} {}km and had delivered {} orders".format
+                (date_today.strftime("%A %d, %B %Y"), total_distance, int(total_deliveries)))
 
 
 mjam = Month()
