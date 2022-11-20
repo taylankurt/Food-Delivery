f775125011b08636c2b8d602a9a9cc67789f4529
Taylan Kurt <kurt.taylan@icloud.com>
2022-11-20 23:08:47 +0100

    average delivery and average distance is added

--- test.py
+++ test.py
@@ -2,8 +2,6 @@ from datetime import datetime as dt, date
 import calendar as cal
 import os
 
-shifts = []
-
 
 class Month:
     def __init__(self):
@@ -25,7 +23,7 @@ class Month:
         month = self.month_number
         date_today = dt.now()
         date_x = date_today.replace(
-            month=month, day=day).strftime("%A %d, %B %Y")
+            month=month, day=day).strftime("""%A %d, %B %Y""")
         return date_x
 
     def last_day_decimal(self):  # to determine the last day of the month
@@ -41,17 +39,31 @@ class Month:
         return largest_number
 
     def shifts_month(self):
+        shifts = []
+        shifts_decimal = []
         for decimal_day in range(self.first_day_decimal(), self.today_decimal() + 1):
             fullday = self.first_day(decimal_day)
             if "Monday" in fullday or "Tuesday" in fullday or "Wednesday" in fullday or "Friday" in fullday or "Saturday" in fullday:
                 shifts.append(fullday)
+                shifts_decimal.append(decimal_day)
         return shifts
+
+    def shifts_month_count(self):
+        shifts = []
+        shifts_decimal = []
+        for decimal_day in range(self.first_day_decimal(), self.today_decimal() + 1):
+            fullday = self.first_day(decimal_day)
+            if "Monday" in fullday or "Tuesday" in fullday or "Wednesday" in fullday or "Friday" in fullday or "Saturday" in fullday:
+                shifts.append(fullday)
+                shifts_decimal.append(decimal_day)
+        return shifts_decimal
 
     def current_distance_delivery(self):
         date_today = dt.now()
         os.system("clear")
         total_distance = 0
         total_deliveries = 0
+
         for x in self.shifts_month():
             daily_distance = float(
                 input("How much did you ride on {}: ".format(x)))
@@ -59,9 +71,16 @@ class Month:
                 input("How many delivers did you had on {}: ".format(x)))
             total_distance = daily_distance + total_distance
             total_deliveries = daily_delivery + total_deliveries
-        return ("You have driven until {} {}km and had delivered {} orders".format
-                (date_today.strftime("%A %d, %B %Y"), total_distance, int(total_deliveries)))
+        av_deliveries = total_deliveries / len(self.shifts_month_count())
+        av_distance = total_distance / len(self.shifts_month_count())
+        return ("Your data for {} is:\nDriven distance: {}km\nAverage distance: {}km\nDelivered orders:  {}\nAverage orders: {}".format
+                (date_today.strftime("%B %Y"), total_distance, av_distance, int(total_deliveries), av_deliveries))
+
+    def write_data(self):
+        f = open("data.txt", "w")
+        f.write(self.current_distance_delivery())
+        f.close()
 
 
 mjam = Month()
-print(mjam.current_distance_delivery())
+print(mjam.write_data())
